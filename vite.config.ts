2c7700e35bd34a55db09553b2319f04bb26404c0
Bekzat <developer.bekzat@gmail.com>
2025-02-25 00:25:36 +0600

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import vercel from 'vite-plugin-vercel';
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vercel()]
+  plugins: [react()],
 })
